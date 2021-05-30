a93eae12c68a1c6630025813a6a7f9d425cf0af1
albert-dm <me@albert-dm.dev>
2021-05-30 17:00:20 -0300

    Configuração do Sqlite

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, g
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+DATABASE = './data/database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+with app.app_context():
+    @app.route('/')
+    def hello_world():
+        cur = get_db().cursor()
+        return 'Hello, World!'
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
