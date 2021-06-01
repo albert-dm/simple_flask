13bdcfbc51fa784c02d322850fdef32489e041d5
mxml2112 <marcoamelo@hotmail.com>
2021-05-31 21:59:33 -0300

    tentativa de instalar flask_sessioin

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
+import os
+
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup, usd
+
+
+
 import sqlite3
 from flask import Flask, g
 app = Flask(__name__)
@@ -16,7 +29,7 @@ with app.app_context():
     @app.route('/')
     def hello_world():
         cur = get_db().cursor()
-        return 'Hello, World!'
+        return 'puta que pariu, Tibeto!'
 
 @app.teardown_appcontext
 def close_connection(exception):
