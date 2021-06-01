7f5caeeac27eeb13e762d6ec5310b82ff12817ce
albert-dm <me@albert-dm.dev>
2021-06-01 10:10:59 -0300

    Configuração do schema
    Ignoring data directory files for security

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import sqlite3
 from flask import Flask, g
+
 app = Flask(__name__)
 
-
 DATABASE = './data/database.db'
+SCHEMA = './data/schema.sql'
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -11,15 +12,26 @@ def get_db():
         db = g._database = sqlite3.connect(DATABASE)
     return db
 
-
-with app.app_context():
-    @app.route('/')
-    def hello_world():
-        cur = get_db().cursor()
-        return 'Hello, World!'
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource(SCHEMA, mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
+@app.route('/')
+def hello_world():
+    with app.app_context():
+        cur = get_db().cursor()
+        return 'Hello, World!'
+
+@app.route('/init')
+def init_app():
+    init_db()
+    return 'Database iniciated'
