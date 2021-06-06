07a26622bfae138f9fe1a693f40ad8b034f09567
albert-dm <contact@albert-dm.dev>
2021-06-06 11:05:21 -0300

    Basic access to MongoDB

--- app.py
+++ app.py
@@ -1,37 +1,21 @@
-import sqlite3
-from flask import Flask, g
+from flask import Flask
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+from bson import json_util
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
-DATABASE = './data/database.db'
-SCHEMA = './data/schema.sql'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource(SCHEMA, mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello_world():
-    with app.app_context():
-        cur = get_db().cursor()
-        return 'Hello, World!'
+    return 'Hello, World!'
 
-@app.route('/init')
-def init_app():
-    init_db()
-    return 'Database iniciated'
+@app.route('/users')
+def get_users():
+    users = list(mongo.db.users.find())
+    return json_util.dumps(users)
